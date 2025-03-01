68921e537796669c7d6a52030fa5439ac8d99bf2
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-03-02 01:04:17 +0600

    fix some minor issues

--- script/blogs.js
+++ script/blogs.js
@@ -1,4 +1,13 @@
-document.getElementById("back-to-desk").addEventListener("click",function(event){
+document.getElementById('back-to-desk').addEventListener('click',function(event){
   event.preventDefault();
   window.location.href = "../index.html";
 })
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   document
+//     .getElementById("back-to-desk")
+//     .addEventListener("click", function (event) {
+//       event.preventDefault();
+//       window.location.href = "../index.html";
+//     });
+// });
